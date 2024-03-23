1c6c80caacafec404b71041ae2850cd61ed65c08
speed <153678863+ttahaa1@users.noreply.github.com>
2024-03-23 02:05:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,24 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
+
 APP_ID = os.environ.get("APP_ID")
 APP_HASH = os.environ.get("APP_HASH")
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
-session = os.environ.get("TERMUX")
 SESSION = os.environ.get("TERMUX")
-token = os.environ.get("TOKEN")
-sython = TelegramClient(StringSession(session), APP_ID, APP_HASH)
-bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
-ispay = ['yes']
-ispay2 = ['yes']
-bot.start()
+TOKEN = os.environ.get("TOKEN")
 
+# تأكد من أن البيانات التي تم تمريرها إلى TelegramClient صحيحة وموجودة
+if APP_ID and APP_HASH and SESSION:
+    sython = TelegramClient(StringSession(SESSION), APP_ID, APP_HASH)
+else:
+    print("تأكد من تعيين متغيرات البيئة APP_ID، APP_HASH، وSESSION بشكل صحيح.")
+
+# تأكد من تعيين المتغيرات البيئية بشكل صحيح
+if APP_ID and APP_HASH and TOKEN:
+    bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=TOKEN)
+else:
+    print("تأكد من تعيين متغيرات البيئة APP_ID، APP_HASH، وTOKEN بشكل صحيح.")
+
+# احذف هذا السطر إذا كنت قد بدأت البوت بالفعل
+# bot.start()
