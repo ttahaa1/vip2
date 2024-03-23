58d8ea5fe65bf0c27ccb303c471c2599f717c8df
speed <153678863+ttahaa1@users.noreply.github.com>
2024-03-23 10:43:00 +0200

    Update sython.py

--- sython.py
+++ sython.py
@@ -144,32 +144,32 @@ async def _(event):
     msg1 = await sython.get_messages('@EEObot', limit=1)
     await msg1[0].click(0)
 
-        chs = 1
-        for i in range(100):
+    chs = 1
+    for i in range(100):
 
-            await asyncio.sleep(5)
+        await asyncio.sleep(5)
 
-            list = await sython(GetHistoryRequest(peer=channel_entity, limit=1,
-                                                   offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
-            msgs = list.messages[0]
-            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
-                await sython.send_message(event.chat_id, f"لايوجد قنوات  في البوت | SY")
-                break
-            url = msgs.reply_markup.rows[0].buttons[0].url
+        list = await sython(GetHistoryRequest(peer=channel_entity, limit=1,
+                                               offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+        msgs = list.messages[0]
+        if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقة مختلفة') != -1:
+            await sython.send_message(event.chat_id, f"لا يوجد قنوات في البوت | SY")
+            break
+        url = msgs.reply_markup.rows[0].buttons[0].url
+        try:
             try:
-                try:
-                    await sython(JoinChannelRequest(url))
-                except:
-                    bott = url.split('/')[-1]
-                    await sython(ImportChatInviteRequest(bott))
-                msg2 = await sython.get_messages('@EEObot', limit=1)
-                await msg2[0].click(text='تحقق')
-                chs += 1
-                
+                await sython(JoinChannelRequest(url))
             except:
-                await sython.send_message(event.chat_id, f"خطأ من المحتمل تم حظر الانضمام ")
-                break
-        await sython.send_message(event.chat_id, "تم الانتهاء من التجميع !")
+                bott = url.split('/')[-1]
+                await sython(ImportChatInviteRequest(bott))
+            msg2 = await sython.get_messages('@EEObot', limit=1)
+            await msg2[0].click(text='تحقق')
+            chs += 1
+            
+        except:
+            await sython.send_message(event.chat_id, f"خطأ من المحتمل أن تم حظر الانضمام ")
+            break
+    await sython.send_message(event.chat_id, "تم الانتهاء من التجميع !")
 
 
 ##################
